c23f59298835df6e7def0e342f2883606a33611a
Brittney Johnson <brittneyejohnson@gmail.com>
2022-07-04 18:18:50 -0400

    Calculate value from difficulty

--- routes/skills.js
+++ routes/skills.js
@@ -12,7 +12,46 @@ module.exports = function (app, db) {
   })
 
   app.post('/skills', (req, res) => {
-    skillsCollection.insertOne(req.body)
+    skill = req.body
+    console.log()
+    let value = 0.0;
+    switch(skill['difficulty']) {
+      case 'A':
+        value = 0.1;
+        break;
+      case 'B':
+        value = 0.2;
+        break;
+      case 'C':
+        value = 0.3;
+        break;
+      case 'D':
+        value = 0.4;
+        break;
+      case 'E':
+        value = 0.5;
+        break;
+      case 'F':
+        value = 0.6;
+        break;
+      case 'G':
+        value = 0.7;
+        break;
+      case 'H':
+        value = 0.8;
+        break;
+      case 'I':
+        value = 0.9;
+        break;
+      case 'I':
+        value = 1.9;
+        break;
+      default:
+        value = 0.0;
+        break;
+    } 
+    skill['value'] = value;
+    skillsCollection.insertOne(skill)
     .then(result => {
       console.log(result)
     })
